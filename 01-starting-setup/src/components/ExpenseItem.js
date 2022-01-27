1af648c9b01bafbc1319644cd39d30371e3bccf6
anuragpatle <anurag.ap6@gmail.com>
2022-01-27 22:30:18 +0530

    Adding css to component

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,11 +1,13 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
 	return (
 
-		<div>
-			<div>Date</div>
-			<div>
-				<h2>Title</h2>
-				<div>Amount</div>
+		<div className="expense-item">
+			<div>March 28th 2021</div>
+			<div className="expense-item__description">
+				<h2>Car Insurance</h2>
+				<div className="expense-item__price" >$340</div>
 			</div>
 		</div>
 
