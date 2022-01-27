09634f4ed6638622a2d9d0d41f06a8bbb233a07f
anuragpatle <anurag.ap6@gmail.com>
2022-01-27 22:41:38 +0530

    accessing dynamic values inside components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+
+	const expenseDate = new Date(2021, 2, 28);
+	const expenseTitle = 'Car Insurance';
+	const expenseAmount = 2234.5;
+
+
 	return (
 
 		<div className="expense-item">
-			<div>March 28th 2021</div>
+			<div>{expenseDate.toISOString()}</div>
 			<div className="expense-item__description">
-				<h2>Car Insurance</h2>
-				<div className="expense-item__price" >$340</div>
+				<h2>{expenseTitle}</h2>
+				<div className="expense-item__price" >${expenseAmount}</div>
 			</div>
 		</div>
 
