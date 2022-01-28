6781d592df19329ac0611cedeac14d9755f47fdc
anuragpatle <anurag.ap6@gmail.com>
2022-01-28 17:28:37 +0530

    Again, splitting components

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -4,7 +4,6 @@ import ExpenseDate from './ExpenseDate';
 function ExpenseItem(props) {
 
 	return (
-
 		<div className="expense-item">
 			<ExpenseDate date={props.date} />
 			<div className="expense-item__description">
