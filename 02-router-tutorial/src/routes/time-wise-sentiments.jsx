e092a7a123e563e11af6e862fa89d84771e9a7ae
anuragpatle <anurag.ap6@gmail.com>
2022-03-07 07:38:25 +0530

    tried to fix google charts issues

--- 02-router-tutorial/src/routes/time-wise-sentiments.jsx
+++ 02-router-tutorial/src/routes/time-wise-sentiments.jsx
@@ -28,31 +28,20 @@ export const options = {
   series: { 5: { type: "line" } },
 };
 
-
 const TimeWiseSentiments = () => {
   return (
     <div>
-      <div class="main-content" style={{ padding: "73px 0px 40px 22px" }}>
-        <div class="fluid-container">
-          <div class="content-wrapper"></div>
-          <div class="page-section" id="about">
-            <div class="row">
-              <div class="col-md-12">
-                <h4 class="widget-title">Today's Timewise Sentiment Records</h4>
-                <div class="about-image">
-                  <Chart
-                    chartType="ComboChart"
-                    width="100%"
-                    height="400px"
-                    data={data}
-                    options={options}
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Chart
+        chartType="ScatterChart"
+        data={[
+          ["Age", "Weight"],
+          [4, 5.5],
+          [8, 12],
+        ]}
+        width="100%"
+        height="400px"
+        legendToggle
+      />
     </div>
   );
 };
